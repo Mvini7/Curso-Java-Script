1360592482c4b2b4b7c3b0f83c965d9f25428af0
Mvini7 <mvinicius030608@gmail.com>
2026-02-02 23:04:23 -0300

    Aula 28

---
+++ Java Script - Básico/Aula 28/script.js
@@ -0,0 +1,12 @@
+//              0       1         2
+const alunos = ['luiz', 'Otavio', 'julia'];
+
+// alunos.unshift('Luiza'); // Adiciona no começo.
+
+// alunos.push('Maria'); // Adiciona um elemento ao final do array.
+// alunos.push('Manuela');
+// alunos.push('Marcos');
+
+// console.log(alunos[0]);
+
+console.log(alunos);
