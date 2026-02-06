fb9fd938534abc19aa1c3f379c608763cd6e2c92
Mvini7 <mvinicius030608@gmail.com>
2026-02-06 01:21:16 -0300

    Aula 44

--- Java script- Logica de Programação/Aula 44/script.js
+++ Java script- Logica de Programação/Aula 44/script.js
@@ -11,17 +11,21 @@ form.addEventListener('submit', function resultadoImc(evento) {
 
   if (IMC < 18.5) {
     resultado.textContent = `Seu IMC é ${IMC.toFixed(2)} e você está abaixo do peso.`;
+    resultado.classList.add('resultado');
   } else if (IMC >= 18.5 && IMC <= 24.99) {
     resultado.textContent = `Seu IMC é ${IMC.toFixed(2)} e você está com peso normal.`;
+    resultado.classList.add('resultado');
   } else if (IMC >= 25 && IMC <= 29.99) {
     resultado.textContent = `Seu IMC é ${IMC.toFixed(2)} e você está com sobrepeso.`;
+    resultado.classList.add('resultado');
   } else if (IMC >= 30 && IMC <= 34.99) {
     resultado.textContent = `Seu IMC é ${IMC.toFixed(2)} e você está com obesidade grau 1.`;
+    resultado.classList.add('resultado');
   } else if (IMC >= 35 && IMC <= 39.99) {
     resultado.textContent = `Seu IMC é ${IMC.toFixed(2)} e você está com obesidade grau 2.`;
+    resultado.classList.add('resultado');
   } else {
     resultado.textContent = `Seu IMC é ${IMC.toFixed(2)} e você está com obesidade grau 3.`;
-  }
-
-  resultado.classList.toggle('resultado');
+    resultado.classList.add('resultado');
+  };
 });
